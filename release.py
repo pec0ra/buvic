223cbbf323e39ff15601754fb03fd9053e8e79fe
Basile Maret <basile.maret@confinale.ch>
2019-12-13 08:01:02 +0100

    Pass version as argument to release.py

--- release.py
+++ release.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+import sys
 
 from script_utils import Colors, p, run_command
-from uv.const import APP_VERSION
 
 
-def make_release():
+def make_release(new_version):
+
+    print(f"* Will create a release for version {new_version}")
+    print()
+
     print("* Checking branch state")
     res = run_command("git status --porcelain", show_std_err=True)
     if res.returncode != 0:
@@ -17,15 +21,10 @@ def make_release():
     res = run_command("git describe --abbrev=0", show_std_err=True)
     last_version = res.stdout.decode().strip()
 
-    new_version = APP_VERSION
-
     if last_version == new_version:
         p("Version number has not changed since last release. Please change it in the file 'version' before running this script.",
           Colors.ERROR)
         exit(1)
-
-    print()
-    print(f"* Will create a release for version {new_version}")
 
     print()
     print("* Creating git tag")
@@ -40,6 +39,9 @@ def make_release():
 
 
 try:
-    make_release()
+    if len(sys.argv) != 2:
+        print("Usage: ./release <version>")
+
+    make_release(sys.argv[1])
 except KeyboardInterrupt:
     print(Colors.ENDC)
