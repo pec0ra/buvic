260f1ce0d93b67f33b5aeaa40e082c65d7ddbec7
Basile Maret <basile.maret@confinale.ch>
2019-12-20 09:52:44 +0100

    Fix cli

--- run_cmd.py
+++ run_cmd.py
@@ -10,11 +10,13 @@ from pprint import PrettyPrinter
 import progressbar
 from matplotlib import rcParams
 
-from buvic.const import DEFAULT_ALBEDO_VALUE, DEFAULT_ALPHA_VALUE, DEFAULT_BETA_VALUE, TMP_FILE_DIR, DEFAULT_OZONE_VALUE
-from buvic.logic.calculation_input import CalculationInput, Settings, Angstrom
+from buvic.const import TMP_FILE_DIR
+from buvic.logic.calculation_input import CalculationInput
 from buvic.logic.calculation_utils import CalculationUtils
 from buvic.logic.file import File
 from buvic.logic.file_utils import FileUtils
+from buvic.logic.parameter_file import Angstrom
+from buvic.logic.settings import Settings
 from buvic.logutils import init_logging
 
 rcParams.update({'figure.autolayout': True})
@@ -24,6 +26,8 @@ pp = PrettyPrinter(indent=2)
 
 DEFAULT_DATA_DIR = "data/"
 DEFAULT_OUTPUT = "out/"
+
+settings = Settings()
 
 parser = ArgumentParser(description="Calculate irradiance spectra")
 group = parser.add_mutually_exclusive_group(required=True)
@@ -43,12 +47,12 @@ group.add_argument("--watch", "-w", action="store_true",
 parser.add_argument("--input-dir", "-i", help="The directory to get the files from")
 parser.add_argument("--output-dir", "-o", help="The directory to save the results in", default=DEFAULT_OUTPUT)
 parser.add_argument("--albedo", "-a", type=float, help="The albedo value to use for the calculations",
-                    default=DEFAULT_ALBEDO_VALUE)
+                    default=settings.default_albedo)
 parser.add_argument("--aerosol", "-e", type=float, nargs=2, metavar=("ALPHA", "BETA"),
-                    default=Angstrom(DEFAULT_ALPHA_VALUE, DEFAULT_BETA_VALUE),
+                    default=settings.default_aerosol,
                     help="The aerosol angstrom's alpha and beta values to use for the calculations.")
 parser.add_argument("--ozone", "-z", type=float, help="The ozone value in DU to use for the calculations if no value is found in a B file",
-                    default=DEFAULT_OZONE_VALUE)
+                    default=settings.default_ozone)
 parser.add_argument("--no-coscor", "-c", help="Don't apply cos correction", action="store_true")
 
 args = parser.parse_args()
@@ -64,6 +68,10 @@ ozone = args.ozone
 output_dir = args.output_dir
 input_dir = args.input_dir
 no_coscor = args.no_coscor
+
+settings.default_albedo = albedo
+settings.default_aerosol = aerosol
+settings.default_ozone = ozone
 
 if not os.path.exists(TMP_FILE_DIR):
     os.makedirs(TMP_FILE_DIR)
@@ -100,12 +108,6 @@ def show_progress(value: float):
 
 if input_dir is None:
     input_dir = DEFAULT_DATA_DIR
-parameters = Settings(
-    albedo,
-    aerosol,
-    ozone,
-    no_coscor
-)
 cmd = CalculationUtils(input_dir, output_dir, init_progress=init_progress, progress_handler=show_progress,
                        finish_progress=finish_progress)
 
@@ -118,7 +120,7 @@ if dates_and_brewer_id is not None:
     date_end = date.fromisoformat(dates_and_brewer_id[1])
     brewer_id = dates_and_brewer_id[2]
 
-    inputs = file_utils.get_calculation_inputs_between(date_start, date_end, brewer_id, parameters)
+    inputs = file_utils.get_calculation_inputs_between(date_start, date_end, brewer_id, settings)
     cmd.calculate_for_inputs(inputs)
 
 elif paths is not None:
@@ -127,7 +129,7 @@ elif paths is not None:
         input_dir = ""
 
     calculation_input = CalculationInput(
-        parameters,
+        settings,
         File(input_dir + paths[0], input_dir),
         File(input_dir + paths[1], input_dir),
         File(input_dir + paths[2], input_dir),
@@ -139,10 +141,10 @@ elif paths is not None:
 elif do_all:
     init_logging(logging.WARN)
 
-    inputs = file_utils.get_calculation_inputs(parameters)
+    inputs = file_utils.get_calculation_inputs(settings)
     cmd.calculate_for_inputs(inputs)
 
 elif watch:
     init_logging(logging.INFO)
 
-    cmd.watch(parameters)
+    cmd.watch(settings)
